9f42904732b817756948110624d3374153144035
Your Name <patilkulkarnirakshita@gmail.com>
2021-07-09 17:39:37 +0530

    some changes done

--- 3_Implementation/src/Title.c
+++
@@ -1,7 +0,0 @@
-#include "hms.h"
-void Title(void)//function for title screen
-{
-	printf("\n\n\t\t----------------------------------------------------------------------------------");
-	printf("\n\t\t\t\t       PK HOSPITAL Management System         ");
-	printf("\n\t\t----------------------------------------------------------------------------------");
-}
